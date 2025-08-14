251571c233a52833dc93c15acd194200adacca89
Anusha T R <Anusha.TR@infineon.com>
2025-08-14 18:09:44 +0530

    ./: Renamed co2 specific files and variables.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- docs/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['cb_2',['cb',['../structxensiv__pasco2__mtb__interrupt__cb__t.html#a9763e6b44be250dc24f7ecc4956b0a8a',1,'xensiv_pasco2_mtb_interrupt_cb_t']]],
-  ['ctx_3',['ctx',['../structxensiv__pasco2__t.html#a1f6d09b30c54854dfca659b7463ac207',1,'xensiv_pasco2_t']]]
-];
--- docs/html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['write_7',['write',['../structxensiv__pasco2__t.html#a66eb18dca204e8839742e71cbb9e6502',1,'xensiv_pasco2_t']]]
-];
